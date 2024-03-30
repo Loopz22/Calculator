4d9f1085b177b109b6402b106712545d87232db0
Loopz <147947278+Loopz22@users.noreply.github.com>
2024-03-30 00:38:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
-let displayValue = '';
+const soundAdd = new Audio('sound.mp3');
+const soundSubtract = new Audio('sound.mp3');
+const soundMultiply = new Audio('sound.mp3');
+const soundDivide = new Audio('sound.mp3');
+const soundEquals = new Audio('sound.mp3');
+const soundClear = new Audio('sound.mp3');
+const soundDelete = new Audio('sound.mp3');
+const soundDigit = new Audio('sound.mp3');
+
+function playSound(operation) {
+    switch (operation) {
+        case '+':
+            soundAdd.play();
+            break;
+        case '-':
+            soundSubtract.play();
+            break;
+        case '*':
+            soundMultiply.play();
+            break;
+        case '/':
+            soundDivide.play();
+            break;
+        case '=':
+            soundEquals.play();
+            break;
+        case 'C':
+            soundClear.play();
+            break;
+        case 'DEL':
+            soundDelete.play();
+            break;
+        default:
+            soundDigit.play();
+    }
+}
+
 
 function appendToDisplay(value) {
     displayValue += value;
     updateDisplay();
-}
-
-function updateDisplay() {
-    document.getElementById('display').value = displayValue;
+    playSound(value);
 }
 
 function clearDisplay() {
     displayValue = '';
     updateDisplay();
+    playSound('C');
 }
 
 function deleteLast() {
     displayValue = displayValue.slice(0, -1);
     updateDisplay();
+    playSound('DEL');
 }
 
 function calculate() {
@@ -24,9 +59,11 @@ function calculate() {
         const result = eval(displayValue);
         displayValue = result.toString();
         updateDisplay();
+        playSound('=');
     } catch (error) {
         displayValue = '';
         updateDisplay();
+        playSound('C');
         alert('Erro na expressão!');
     }
 }
